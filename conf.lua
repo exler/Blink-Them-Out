9349fba9497638b73838c13c40e5e3efb56c0d2e
exler <kakkmar@gmail.com>
2017-12-01 13:27:00 +0100

    Create love config

---
+++ conf.lua
@@ -0,0 +1,19 @@
+function love.conf(t)
+    -- global variables
+    windowScale = 3
+    windowWidth = 480
+    windowHeight = 270
+
+    t.version = "0.10.2"
+    t.console = false
+
+    t.window.title = "VAFG"
+    t.window.icon = nil
+
+    t.window.width = windowWidth * windowScale
+    t.window.height = windowHeight * windowScale
+    t.window.minwidth = windowWidth
+    t.window.minheight = windowHeight
+    t.window.resizable = false
+    t.window.fullscreen = false
+end
