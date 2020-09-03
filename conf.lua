f6796d70dc39532c9bb8e1d16650c7cd7ba18ddc
Kamil Marut <kakkmar@gmail.com>
2020-09-03 13:28:55 +0200

    Bump Love2D version

--- conf.lua
+++ conf.lua
@@ -2,7 +2,7 @@ function love.conf(t)
     t.window.width = 1188
     t.window.height = 764
 
-    t.version = "0.10.2"
+    t.version = "11.3"
     t.console = true
 
     t.window.title = "Blink Them Out"
